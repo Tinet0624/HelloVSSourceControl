05d29a73fe0635992a05dcc4183dd6dab0dfc092
Alexandra Haley <72041575+Tinet0624@users.noreply.github.com>
2021-07-07 15:27:09 -0700

    Removed unused main method perm.

--- HelloVSSourceControl/Program.cs
+++ HelloVSSourceControl/Program.cs
@@ -8,7 +8,7 @@ namespace HelloVSSourceControl
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Welcome to the wonderful world of GitHub!");
 
