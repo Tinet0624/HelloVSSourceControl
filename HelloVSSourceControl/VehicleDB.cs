ebd6daa95a9f5724e4a07df43857f7effe65bcb6
Alexandra Haley <72041575+Tinet0624@users.noreply.github.com>
2021-07-18 13:00:06 -0700

    Created VehicleDB class with stub

---
+++ HelloVSSourceControl/VehicleDB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloVSSourceControl
+{
+    /// <summary>
+    /// DB helper class for Vehicle CRUD functionality.
+    /// </summary>
+    static class VehicleDB
+    {
+        /// <summary>
+        /// Get a list of vehicles that are in stock.
+        /// </summary>
+        /// <returns></returns>
+        public static List<string> GetAllInStockVehicles()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
