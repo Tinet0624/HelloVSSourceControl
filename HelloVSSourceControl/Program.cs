28e3bd4e68f72fde620229d0166b696e127e189f
Alexandra Haley <72041575+Tinet0624@users.noreply.github.com>
2021-07-17 17:46:02 -0700

    Changed welcome message and added a display message for the in stock cars.

--- HelloVSSourceControl/Program.cs
+++ HelloVSSourceControl/Program.cs
@@ -10,10 +10,29 @@ namespace HelloVSSourceControl
     {
         static void Main()
         {
-            Console.WriteLine("Welcome to the wonderful world of GitHub!");
+            Console.WriteLine("Welcome to the dealership manager!");
+
+            DisplayInStockCars();
 
             Console.WriteLine("Press a key to end.");
             Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Displays a list of instock cars in alphabetical order.
+        /// </summary>
+        static void DisplayInStockCars()
+        {
+            // Not pulling form a Database yet
+            List<string> inStockCars = new List<string>
+            {
+                "Honda Prelude - Qty: 5",
+                "Kia Forte - Qty: 10"
+            };
+            foreach (string currCar in inStockCars)
+            {
+                Console.WriteLine(currCar);
+            }
         }
     }
 }
