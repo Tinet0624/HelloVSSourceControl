5f803af929fef3ce1718a58c628cd09684ac1773
Alexandra Haley <72041575+Tinet0624@users.noreply.github.com>
2021-07-18 12:47:16 -0700

    Formated DB with whitespace

--- VehicleInventoryDB/VehicleInventoryDB.sql
+++ VehicleInventoryDB/VehicleInventoryDB.sql
@@ -6,7 +6,7 @@ GO
 
 CREATE TABLE Vehicle
 (
-    Make varchar(30) NOT NULL,
-    Model varchar(30) NOT NULL,
-    Quantity tinyint DEFAULT 0
+    Make      varchar(30) NOT NULL,
+    Model     varchar(30) NOT NULL,
+    Quantity  tinyint     DEFAULT 0
 )
